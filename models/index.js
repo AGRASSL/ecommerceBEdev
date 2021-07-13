88f6c3366423af102c9660f5fff184658bffe9fa
Alexandra Grassl <eai.alexandra@gmail.com>
2021-07-13 13:18:27 -0500

    updated config file

--- models/index.js
+++ models/index.js
@@ -6,11 +6,33 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  through: {
+    foreignKey: 'category_id',
+  },
+
+  as: 'products_tag'
+});
+
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tags, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Products, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+});
 
 module.exports = {
   Product,
